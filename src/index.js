a7d6b4b4d0bc286ab8de06ef43343eece96a431d
Igor Bezanovic <i.bezanovic@levi9.com>
2022-03-04 14:50:12 +0100

    Node progress

--- src/index.js
+++ src/index.js
@@ -1,29 +1,125 @@
 const express = require('express');
+var cors = require('cors')
 require('./db/mongoose');
 const User = require('./models/user');
 const Training = require('./models/training');
 
 const app = express();
-const port = process.env.PORT || 8080;
-
+app.use(cors())
 app.use(express.json());
 
-app.post('/users', (req, res) => {
-    const user = new User(req.body)
-    user.save().then(()=> {
+const port = process.env.PORT || 8080;
+
+app.post('/users', async (req, res) => {
+    const user = new User(req.body);
+    try{
+        await user.save()
         res.send(user)
-    }).catch((error)=>{
+    } catch(error) {
         res.status(400).send(error)
-    })
+    }
 })
 
-app.post('/training', (req, res) => {
-    const training = new Training(req.body)
-    training.save().then(()=> {
-        res.send(training)
-    }).catch((error)=>{
+app.get('/users', async (req, res) => {
+    try{
+        const user = await User.find({})
+        res.send(user)
+    } catch(error) {
+        res.status(500).send(error)
+    }
+})
+
+app.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try{
+        const user = await User.findById(_id)
+        if(!user){
+            return res.status(404).send()
+        } else{
+            res.send(user)
+        }
+    } catch(error) {
+        res.status(500).send(error)
+    }
+})
+
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperations = updates.every((update) => allowedUpdates.includes(update)) 
+
+    if(!isValidOperations){
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true,  runValidators: true })
+        if(!user){
+            return res.status(404).send()
+        } else {
+            res.send(user)
+        }
+    } catch(error) {
         res.status(400).send(error)
-    })
+    }
+})
+
+app.post('/training', async (req, res) => {
+    const training = new Training(req.body)
+    try{
+        await training.save()
+        res.send(training)
+    } catch(error) {
+        res.status(500).send(error)
+    }
+})
+
+app.get('/training', async (req, res) => {
+    try{
+        const training = await Training.find({})
+        if(!training){
+            return res.status(404).send()
+        } else {
+            res.send(training)
+        }
+    } catch(error) {
+        res.status(500).send(error)
+    }
+})
+
+app.get('/training/:id', async (req, res) => {
+    const _id = req.params.id
+    try{
+        const training = await Training.findById(_id);
+        if(!training) {
+            return res.status(404).send()
+        } else {
+            res.send(training)
+        }
+    } catch(error) {
+        res.status(500).send(error)
+    }
+})
+
+app.patch('/training/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperations = updates.every((update) => allowedUpdates.includes(update)) 
+
+    if(!isValidOperations){
+        return res.status(400).send({ error: 'Invalid updates' })
+    }
+    
+    try{
+        const training = await Training.findByIdAndUpdate(req.params.id, req.body, { new: true,  runValidators: true })
+        if(!training){
+            return res.status(404).send()
+        } else {
+            res.send(training)
+        }
+    } catch(error) {
+        res.status(400).send(error)
+    }
 })
 
   app.post("/post", (req, res) => {
